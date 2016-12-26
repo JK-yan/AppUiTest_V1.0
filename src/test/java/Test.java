94da873e8624cc3181710a70413e9a7bd3b88aef
Jackiezero <461831455@qq.com>
2016-12-27 00:39:24 +0800

    a

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,14 +1,25 @@
-import FileUtils.TimeUtils;
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.List;
 
 /**
  * Created by jackiezero on 2016/12/26.
  */
 public class Test {
-
+    @SerializedName(value = "emailAddress", alternate = {"email", "email_address"})
+    public String emailAddress;
+    public String name;
+    public int age;
     public static void main(String[] args){
-        TimeUtils timeUtils = new TimeUtils();
-timeUtils.GetLocalTime();
-        timeUtils.gess();
-
+        Gson gson = new Gson();
+        String json = "{\"name\":\"怪盗kidou\",\"age\":24,\"emailAddress\":\"ikidou_1@example.com\",\"email\":\"ikidou_2@example.com\",\"email_address\":\"ikidou_3@example.com\"}";
+        Test user = gson.fromJson(json, Test.class);
+        System.out.println(user.emailAddress+user.age); // ikidou_3@example.com
+        String jsonArray = "[\"Android\",\"Java\",\"PHP\"]";
+        String[] strings = gson.fromJson(jsonArray, String[].class);
+        List<String> stringList = gson.fromJson(jsonArray, new TypeToken<List<String>>() {}.getType());
+        System.out.print(stringList);
     }
 }
