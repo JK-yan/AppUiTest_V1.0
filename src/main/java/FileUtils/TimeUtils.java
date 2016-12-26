18b9a607cdd25810eec7e4d67a029f8446878468
Jackiezero <461831455@qq.com>
2016-12-27 00:15:14 +0800

    Utils

---
+++ src/main/java/FileUtils/TimeUtils.java
@@ -0,0 +1,35 @@
+package FileUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by jackiezero on 2016/12/26.
+ */
+public class TimeUtils {
+
+    public String GetLocalTime(){
+        String timeformat="yyyy/MM/dd HH:mm:ss";
+        Date now = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat(timeformat);
+        String hehe = dateFormat.format(now);
+        System.out.println(hehe);
+        System.out.println(dateFormat);
+        return hehe;
+    }
+    public void  gess(){
+        Calendar c = Calendar.getInstance();//可以对每个时间域单独修改
+
+
+
+
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int date = c.get(Calendar.DATE);
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+        int second = c.get(Calendar.SECOND);
+        System.out.println(year + "/" + month + "/" + date + " " +hour + ":" +minute + ":" + second);
+    }
+}
---
+++ src/test/java/Test.java
@@ -0,0 +1,14 @@
+import FileUtils.TimeUtils;
+
+/**
+ * Created by jackiezero on 2016/12/26.
+ */
+public class Test {
+
+    public static void main(String[] args){
+        TimeUtils timeUtils = new TimeUtils();
+timeUtils.GetLocalTime();
+        timeUtils.gess();
+
+    }
+}
