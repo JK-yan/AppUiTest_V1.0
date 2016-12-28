85f50f0b0d9ccccc83bb2a7828a3c1af34a02034
Jackiezero <461831455@qq.com>
2016-12-29 00:33:30 +0800

    /**  * 解决读取Yaml文件问题  * @load读取所有单类  * @LoadgetValue根据key读取value  * @Loadall 读取多层次Yaml文件  * @Loadallgetvalue根据List下标和key读取value  */

--- src/main/java/Utils/YamlUtils.java
+++ src/main/java/Utils/YamlUtils.java
@@ -6,16 +6,26 @@ import org.yaml.snakeyaml.Yaml;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 
 /**
  * Created by admin on 2016/12/28.
+ * 读取Yaml文件
+ * @load读取所有单类
+ * @LoadgetValue根据key读取value
+ * @Loadall 读取多层次Yaml文件
+ * @Loadallgetvalue根据List下标和key读取value
  */
 public class YamlUtils {
     private static Logger logger = Logger.getLogger(YamlUtils.class);
-    public static Object load(){
+
+    public static Object Load(){
         Yaml yaml = new Yaml();
         File f=new File(FileUtils.Path("/src/main/resources/test.yaml"));
+
         //读入文件
         Object result= null;
         try {
@@ -28,40 +38,46 @@ public class YamlUtils {
         return result;
 
     }
-    /*public static void aa(){
-        Yaml yaml = new Yaml();
-        File f=new File(FileUtils.Path("/src/main/resources/test.yaml"));
-        //读入文件
-        String result= null;
-        try {
-            result = yaml.addImplicitResolver(new Tag("!dice"), Pattern.compile(), "123456789");
-        } catch (FileNotFoundException e) {
-            logger.error(e);
-            e.printStackTrace();
-        }
-        logger.info(result);
-//        return result;
 
-    }*/
-    public static Iterable<Object> loadall(){
+    public static Object LoadgetValue(String ...te){
+
+        LinkedHashMap aa= (LinkedHashMap) Load();
+        for (int i=0;i<te.length-1;i++){
+            aa = (LinkedHashMap) aa.get(te[i]);
+        }
+
+        return  aa.get(te[te.length-1]);
+    }
+    public static List Loadall(){
         Yaml yaml = new Yaml();
         File f = new File(FileUtils.Path("/src/main/resources/test.yaml"));
         Iterable<Object> result = null;
+        //Object object = null;
+        List list =new ArrayList();
         try {
             result = yaml.loadAll(new FileInputStream(f));
+
+            for (Object obj:result){
+                //object=obj;
+                list.add(obj);
+            }
         } catch (FileNotFoundException e) {
 
             logger.error(e);
             e.printStackTrace();
         }
-        /*for (Object obj : result) {
-            System.out.println(obj.getClass());
-            System.out.println(obj);
-        }*/
-        logger.info(result);
 
-        return result;
+        logger.info(list);
+
+        return list;
     }
+    public static Object Loadallgetvalue(int a,String ...te){
+        LinkedHashMap aa= (LinkedHashMap) Loadall().get(a);
+        for (int i=0;i<te.length-1;i++){
+            aa = (LinkedHashMap) aa.get(te[i]);
+        }
 
+        return  aa.get(te[te.length-1]);
+    }
 
 }
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,8 +1,5 @@
 import Utils.YamlUtils;
 import com.google.gson.annotations.SerializedName;
-import org.apache.log4j.Logger;
-
-import java.util.LinkedHashMap;
 
 /**
  * Created by jackiezero on 2016/12/26.
@@ -12,22 +9,29 @@ public class Test {
     public String emailAddress;
     public String name;
     public int age;
-    LinkedHashMap aa;
-    LinkedHashMap obj;
-    private static Logger logger = Logger.getLogger(Test.class);
-    public Test(){
-        aa= (LinkedHashMap) YamlUtils.load();
-    }
-    public Test getValue(String key){
-        obj = (LinkedHashMap) aa.get(key);
-        return this;
-    }
-    public String get(String key){
-        return obj.get(key).toString();
-    }
+    //LinkedHashMap aa;
+    //LinkedHashMap obj;
+    //private static Logger logger = Logger.getLogger(Test.class);
+    //public Test(){
+    //    aa= (LinkedHashMap) YamlUtils.load();
+    //}
+    //public Test getValue(String key){
+    //    obj = (LinkedHashMap) aa.get(key);
+    //    return this;
+    //}
+    //public String get(String key){
+    //    return obj.get(key).toString();
+    //}
+
     public static void main(String[] args){
-        Test aa=new Test();
+
+        //getValue("spring","datasource","driverClassName");
+        //System.out.println(getValue("spring"));
+        //Object aa=YamlUtils.load();
+        //System.out.println(aa.toString());
 //        System.out.println(aa.getValue("spring").getValue("datasource").getValue("url"));
-        System.out.println(aa.getValue("spring").getValue("datasource").get("url"));
+//        System.out.println(YamlUtils.LoadallgetValue("spring","application","cruncher"));
+//        System.out.println(YamlUtils.Loadallgetvalue("spring","application","cruncher"));
+        System.out.println(YamlUtils.Loadallgetvalue(1,"spring","application","name"));
     }
 }
